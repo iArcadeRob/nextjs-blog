eb5bacef9a2fea382fc7f5d125cc29e3418e65ae
Rob Gill <rgill@ipipeline.com>
2020-11-09 18:35:08 -0500

    remove https

--- pages/local.js
+++ pages/local.js
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 export default function Local() {
-    const { data, error } = useSWR('https://xkcd.com/4/info.0.json', fetcher)
+    const { data, error } = useSWR('//xkcd.com/4/info.0.json', fetcher)
     if (error) return <div>failed to load</div>
     if (!data) return <div>loading...</div>
     // render data
